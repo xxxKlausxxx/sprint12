e7bd24cc4c1df07a4fb86e6877072355ed0d972e
Nazarov <79166357230@yandex.ru>
2020-09-27 12:32:10 +0300

    Подключение роутов

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,29 @@
-const usersRouter = require('express').Router();
-module.exports = usersRouter;
+const router = require('express').Router();
 const users = require('../data/users.json');
 
-usersRouter.get('/users', (req, res) => {
-  res.send(users)
-})
+const readUsers = async (req, res) => {
+  try {
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({ message: 'Ошибка чтения файла пользователей' });
+  }
+};
+
+const readUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const userSought = users.find((item) => item._id === id);
+    if (!userSought) {
+      res.status(404).send({ message: 'Нет пользователя с таким id' });
+    } else {
+      res.status(200).send(userSought);
+    }
+  } catch (err) {
+    res.status(500).send({ message: 'Ошибка чтения файла пользователей' });
+  }
+};
+
+router.get('/users', readUsers);
+router.get('/users/:id', readUserById);
+module.exports = router;
